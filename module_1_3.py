227e7a7663dd9f45a71571759057b3fb562ceeb3
blewseed <blewseed757@gmail.com>
2024-10-09 23:33:48 +0900

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+#практическое задание: Динамическая типизация
+name='Julia'
+print("Name:",name)
+age=34
+print("Age: ",age)
+print("New age: ", age+2)
+is_student=True
+print("Is Student: ", is_student)
